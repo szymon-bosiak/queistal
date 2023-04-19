32d63944d8bf0ea2c5a06c320562ce4cae0dd496
Szymon Bosiak <szymon.bosiak.99@gmail.com>
2023-04-19 20:16:37 +0200

    Finished making content

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 import './footer.css'
+import phone from '../../assets/phone.svg'
+import mail from '../../assets/mail.svg'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <div className='footer-decor'>
+        <p>© 2023:</p>
+        <p>- Built by <br /> Szymon Bosiak</p>
+        <p>- Design inspierd by Ronas IT</p>
+      </div>
+
+      <div className='footer_content'>
+
+        <div className='footer_content-legal'>
+          <h4>"QUEIS TAL" Sp z o.o.</h4>
+          <p>67-320 Małomice, ul. Kościuszki 19c</p>
+        </div>
+
+        <div className='footer_content-phone'>
+          <img src={phone} alt="phone icon" />
+          <h4>782 243 640</h4>
+        </div>
+
+        <div className='footer_content-mail'>
+          <img src={mail} alt="e-mail icon" />
+          <h4>queistal@gmail.com</h4>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
--- src/components/Recomendations/Recomendations.jsx
+++ src/components/Recomendations/Recomendations.jsx
@@ -1,34 +1,100 @@
 import React from 'react'
 import './recomendations.css'
-import profile from '../../assets/profile.ico'
+import man_suit from '../../assets/avatars/man_suit.svg'
+import man_balding from '../../assets/avatars/man_balding.svg'
+import woman_glasses from '../../assets/avatars/woman_glasses.svg'
+import man_blouse from '../../assets/avatars/man_blouse.svg'
+
+import woman_elegant from '../../assets/avatars/woman_elegant.svg'
+import man_tophat from '../../assets/avatars/man_tophat.svg'
+import woman_blonde from '../../assets/avatars/woman_blonde.svg'
 
 const Recomendations = () => {
-  return (
-      <div className='recomendations_container'>
-          <div className='recomendations_container-flex'>
-              <h3>//REKOMENDACJE</h3>
 
-              <div className='swiper-flex'>
-                  <div className='swiper'>
-                    <div className='swiper-triangle-upper'></div>
-                    <img src={profile} alt="" />
-                    <h3>Janusz Zapomoga</h3>
-                      <p>Chciałbym podzielić się moim wspaniałym doświadczeniem z firmą budowlaną XYZ. 
-                        Zleciłem im wykonanie kompleksowej renowacji mojego domu i jestem zachwycony 
-                        efektem. Od samego początku praca z nimi była przyjemnością. Zawsze mogłem na 
-                        nich liczyć, gdy potrzebowałem pomocy i doradztwa. Wszystkie prace wykonywane 
-                        były terminowo i z dbałością o każdy szczegół. Ich podejście do klienta i 
-                        elastyczność były nieocenione, co pozwoliło mi poczuć się pewnie w trudnych 
-                        momentach. Efekt końcowy przekroczył moje oczekiwania, a moje nowe, piękne i 
-                        funkcjonalne wnętrze, to w dużej mierze ich zasługa. Z całego serca polecam tę 
-                        firmę każdemu, kto szuka nie tylko wysokiej jakości usług budowlanych, ale 
-                        także indywidualnego podejścia i zaangażowania.</p>
-                    <div className='swiper-triangle-lower'></div>
-                  </div>
-              </div>
+  const reviews = [
+    {
+      img: man_suit,
+      name: 'Robert Woźniak',
+      position: 'Minister',
+      txt: 'Jestem zachwycony z wykonanej przez firmę budowlaną drewnianej altany w moim ogrodzie. Cały projekt był zrealizowany bardzo profesjonalnie, a ekipa pracownicza była bardzo kompetentna i dokładna. Altana jest pięknie wykonana, z dbałością o każdy szczegół i doskonale wpasowała się w styl mojego ogrodu. Jestem bardzo zadowolony z efektu końcowego i polecam tę firmę każdemu, kto szuka solidnego wykonawcy.'
+    },
+    {
+      img: woman_blonde,
+      name: 'Julia Sadowska',
+      position: '',
+      txt: 'Zamówiłam w firmie budowlanej wykonanie dwóch drewnianych ławek z grawerunkiemi jestem bardzo zadowolona z efektu końcowego. Ławki są pięknie wykonane, z dbałością o każdy szczegół, a grawerunek dodaje im wyjątkowego charakteru.'
+    },
+    {
+      img: woman_glasses,
+      name: 'Asia Kozłowska',
+      position: '',
+      txt: 'Firma "Queis Tal" zrobiła wspaniałą robotę przy budowie mojego domu. Ich zespół był bardzo zaangażowany i dbał o każdy szczegół. Wszystkie prace były wykonywane terminowo i zgodnie z planem. Polecam tę firmę każdemu, kto szuka nie tylko wysokiej jakości usług budowlanych, ale także zaangażowania i elastyczności.'
+    },
+    {
+      img: man_blouse,
+      name: 'Janusz Zapomoga',
+      position: '',
+      txt: 'Jestem bardzo zadowolony z usług firmy. Ich podejście do klienta jest niesamowite, a praca wykonana była bardzo dokładnie i z dbałością o każdy szczegół. Efekt końcowy przekroczył moje oczekiwania, a cały projekt był zakończony szybciej niż się spodziewałem. Polecam tę firmę każdemu, kto szuka profesjonalnego i rzetelnego wykonawcy.'
+    },
+    {
+      img: woman_elegant,
+      name: 'Maria Kubiak',
+      position: '',
+      txt: 'Zespół prawdziwych profesjonalistów. Wykonali dla mnie renowację domu, a ich praca była szybka, efektywna i dokładna. Polecam ich usługi z czystym sumieniem.'
+    },
+    {
+      img: man_tophat,
+      name: 'Jacek Witkowski',
+      position: '',
+      txt: 'Firma zrobiła fantastyczną robotę przy remoncie mojego mieszkania. Byli bardzo profesjonalni i zawsze dostępni, gdy potrzebowałem pomocy. Ich praca była bardzo dokładna, a efekt końcowy przekroczył moje oczekiwania. Polecam z całego serca!'
+    },
+    {
+      img: man_balding,
+      name: 'Leszek Kowalczyk',
+      position: '',
+      txt: 'Bardzo polecam. Byli niesamowicie elastyczni i zawsze stawiali moje potrzeby na pierwszym miejscu. Ich praca była bardzo szybka i efektywna, a efekt końcowy był dokładnie taki, jakiego oczekiwałem. Na pewno skorzystam z ich usług ponownie.'
+    },
+  ]
+
+  return (
+    <div className='recomendations_container'>
+      <div className='recomendations_container-flex'>
+        <h3>//REKOMENDACJE</h3>
+
+        <div className='swiper-flex'>
+          <div className='swiper'>
+            <div className='swiper-triangle-upper'></div>
+            <div className='swiper-triangle-lower'></div>
+
+            <swiper-container pagination="true" pagination-clickable="true" navigation="true" space-between="30"
+              centered-slides="true" autoplay-delay="15000" autoplay-disable-on-interaction="false" style={{
+                "--swiper-navigation-color": "#E3EBD4",
+                "--swiper-pagination-color": "#E3EBD4",
+              }}>
+
+              {reviews.map((review, index) => {
+                return (
+                  <swiper-slide key={index}>
+                    <div className='swiper-content'>
+                      <div className='swiper-content-inner'>
+                        <img src={review.img} alt="avatar" />
+                        <div className='swiper-content-inner-name'>
+                          <h2>{review.name}</h2>
+                          <p>{review.position}</p>
+                        </div>
+                      </div>
+                      <p className='swiper-content-inner-desc'>{review.txt}</p>
+                    </div>
+                  </swiper-slide>
+                )
+              })}
+
+            </swiper-container>
 
           </div>
+        </div>
       </div>
+    </div>
   )
 }
 
