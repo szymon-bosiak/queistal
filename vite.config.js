1a525204f87ee111b654d54468b0fa2f682054e6
szymon-bosiak <szymon.bosiak.99@gmail.com>
2024-07-05 15:42:37 +0200

    chore: changed base url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://queistal.pl/',
+  base: 'https://szymon-bosiak.github.io/queistal/',
   plugins: [react()],
 })
